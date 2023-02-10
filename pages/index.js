1000756bf81caf20666d2674872afafd8ec361ef
ailsakasyfa <125033150+ailsakasyfa@users.noreply.github.com>
2023-02-10 21:16:05 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,14 @@ export default function Home({ posts }) {
             </h2>
           </div>
         </div>
+        <p className="space-y-4 text-zinc-500 dark:text-slate-300">
+          <p className="my-4 leading-loose">
+            Currently pursuing a comprehensive four-year course of study in MICE
+            (Meeting, Incentive, Convention and Exhibition). Have strong teamwork 
+            capabilities and knowledge of MICE project management. Committed to continuous
+            learning and professional development within the event management field.
+          </p>
+        </p>
         <div className="mt-8 flex flex-wrap gap-4 text-center">
           <a
             className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-blue-500 py-2 px-2.5 font-medium text-white shadow-lg shadow-blue-500/10 transition selection:bg-white/30 hover:bg-blue-500/80 hover:shadow-blue-500/5 focus:ring-blue-500/40 dark:bg-blue-400 dark:text-zinc-900 dark:shadow-blue-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-blue-400/80 dark:hover:shadow-blue-400/5 dark:focus:ring-blue-400/40 sm:w-auto sm:px-3 sm:pl-2.5"

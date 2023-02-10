5b6d63fb2a672fa638bdb4779388f213a364d898
ailsakasyfa <125033150+ailsakasyfa@users.noreply.github.com>
2023-02-10 21:30:54 +0700

    Add files via upload

---
+++ data/featuredprojectsData.js
@@ -0,0 +1,10 @@
+const featuredprojectsData = [
+  {
+    title: 'Convention Management Certification',
+    description: `American Hotel & Lodging Educational Institute Certification`,
+    imgSrc: '/static/images/sertifikasi.jpeg',
+    href: '/blog/sertifikasi',
+  }
+]
+  
+export default featuredprojectsData

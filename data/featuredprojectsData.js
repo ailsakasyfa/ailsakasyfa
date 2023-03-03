526bdbe9d15bbac5002d792dda34181d00251366
ailsakasyfa <125033150+ailsakasyfa@users.noreply.github.com>
2023-03-03 21:48:01 +0700

    Update featuredprojectsData.js

--- data/featuredprojectsData.js
+++ data/featuredprojectsData.js
@@ -4,7 +4,13 @@ const featuredprojectsData = [
     description: `American Hotel & Lodging Educational Institute Certification`,
     imgSrc: '/static/images/sertifikasi.jpeg',
     href: '/blog/sertifikasi',
+  },
+   {
+    title: 'From Hesitant to Enriched',
+    description: `My Experience in a Virtual Exchange Program with SEAMEO TVET`,
+    imgSrc: '/static/images/seameo.jpg',
+    href: '/blog/seameo',
   }
 ]
-  
+
 export default featuredprojectsData

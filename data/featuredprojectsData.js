0a4d9df0fb75660bbfbb438d5e6527a09e3b239c
ailsakasyfa <125033150+ailsakasyfa@users.noreply.github.com>
2023-03-23 20:07:20 +0700

    Update featuredprojectsData.js

--- data/featuredprojectsData.js
+++ data/featuredprojectsData.js
@@ -1,9 +1,27 @@
 const featuredprojectsData = [
   {
+    title: 'Indonesian International Student Mobility Award 2022',
+    description: `My Study Abroad Experience in South Korea`,
+    imgSrc: '/static/images/IISMA.jpg',
+    href: '/blog/iisma-2022',
+  },
+  {
     title: 'Banquet Server Lotte Hotel and Resort Ulsan',
     description: `A Cultural Immersion. My Experience Working in the Hospitality Industry in South Korea`,
     imgSrc: '/static/images/Banquet Server Lotte Hotel and Resort Ulsan.jpg',
     href: '/blog/lotte-hotel-and-resort-ulsan',
+  },
+  {
+    title: 'Teaching Management Training and Organization',
+    description: `Lessons Learned as a Project Officer`,
+    imgSrc: '/static/images/PMPO.JPG',
+    href: '/blog/teaching-management-training-and-organization',
+  },
+  {
+    title: 'Fall in Kansai Trip',
+    description: `A Tour Leader's Perspective on Planning a Memorable Trip to Japan`,
+    imgSrc: '/static/images/fall in kansai.JPG',
+    href: '/blog/fall-in-kansai',
   },
   {
     title: 'Chao Khru Thailand and Vietnam Trip',
@@ -12,6 +30,7 @@ const featuredprojectsData = [
     href: '/blog/chao-khru',
   },
 ]
+
 
 
 
